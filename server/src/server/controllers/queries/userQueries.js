6532e990933cb0a2b825a6ec56ef05064ab7ed70
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-27 22:36:42 +0300

    Add user delete db

--- server/src/server/controllers/queries/userQueries.js
+++ server/src/server/controllers/queries/userQueries.js
@@ -1,29 +1,32 @@
-const bd = require('../../models/index');
+const bcrypt = require('bcrypt');
+const { User } = require('../../models/index');
 const NotFound = require('../../errors/UserNotFoundError');
 const ServerError = require('../../errors/ServerError');
-const bcrypt = require('bcrypt');
 
 module.exports.updateUser = async (data, userId, transaction) => {
-  const [updatedCount, [updatedUser]] = await bd.Users.update(data,
-    { where: { id: userId }, returning: true, transaction });
+  const [updatedCount, [updatedUser]] = await User.update(data, {
+    where: { id: userId },
+    returning: true,
+    transaction,
+  });
   if (updatedCount !== 1) {
     throw new ServerError('cannot update user');
   }
-  return updatedUser.dataValues;
+  return updatedUser.get({ plain: true });
 };
 
 module.exports.findUser = async (predicate, transaction) => {
-  const result = await bd.Users.findOne({ where: predicate, transaction });
-  if ( !result) {
+  const result = await User.findOne({ where: predicate, transaction });
+  if (!result) {
     throw new NotFound('user with this data didn`t exist');
   } else {
     return result.get({ plain: true });
   }
 };
 
-module.exports.userCreation = async (data) => {
-  const newUser = await bd.Users.create(data);
-  if ( !newUser) {
+module.exports.userCreation = async data => {
+  const newUser = await User.create(data);
+  if (!newUser) {
     throw new ServerError('server error on user creation');
   } else {
     return newUser.get({ plain: true });
@@ -32,7 +35,7 @@ module.exports.userCreation = async (data) => {
 
 module.exports.passwordCompare = async (pass1, pass2) => {
   const passwordCompare = await bcrypt.compare(pass1, pass2);
-  if ( !passwordCompare) {
+  if (!passwordCompare) {
     throw new NotFound('Wrong password');
   }
 };
