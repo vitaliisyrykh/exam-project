d69570d4c58f2b5fe4fa6bad4f3d2dc896acba00
Vladimir <freshcode.yt@gmail.com>
2021-04-29 15:54:24 +0300

    Add authService

--- server/src/server/controllers/authController.js
+++ server/src/server/controllers/authController.js
@@ -1,34 +1,18 @@
 const createHttpError = require('http-errors');
-const _ = require('lodash');
 const { User, RefreshToken } = require('../models');
-const JWTService = require('../services/jwtService');
-const CONSTANTS = require('./../../constants');
+const AuthService = require('../services/authService');
 
 module.exports.signUp = async (req, res, next) => {
   try {
     const { body } = req;
     const user = await User.create(body);
     if (user) {
-      /* Create token pair */
-      const tokenPair = await JWTService.createTokenPair({
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      });
-      await user.createRefreshToken({ value: tokenPair.refreshToken });
-      /* prepare user */
-      const preparedUser = _.omit(user.get(), ['password']);
-      /* Send user and tokenPair */
-      return res.send({
-        data: {
-          user: preparedUser,
-          tokenPair,
-        },
-      });
+      const data = await AuthService.createSession(user);
+      return res.send({ data });
     }
     next(createHttpError(400, 'Unable to create user with provided data'));
   } catch (error) {
-    next(err);
+    next(error);
   }
 };
 
@@ -40,33 +24,9 @@ module.exports.signIn = async (req, res, next) => {
     const user = await User.findOne({
       where: { email },
     });
-    /* Compare passwords */
     if (user && (await user.comparePassword(password))) {
-      /* Create token pair */
-      const tokenPair = await JWTService.createTokenPair({
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      });
-      if ((await user.countRefreshTokens()) <= CONSTANTS.MAX_DEVICES_AMOUNT) {
-        await user.createRefreshToken({ value: tokenPair.refreshToken });
-      } else {
-        const [oldRefreshToken] = await user.getRefreshTokens({
-          order: [['updatedAt', 'ASC']],
-        });
-        await oldRefreshToken.update({
-          value: tokenPair.refreshToken,
-        });
-      }
-      /* prepare user */
-      const preparedUser = _.omit(user.get(), ['password']);
-      /* Send user and tokenPair */
-      return res.send({
-        data: {
-          user: preparedUser,
-          tokenPair,
-        },
-      });
+      const data = await AuthService.createSession(user);
+      return res.send({ data });
     }
     next(createHttpError(401, 'Invalid credentials'));
   } catch (error) {
@@ -79,32 +39,11 @@ module.exports.refreshToken = async (req, res, next) => {
     const {
       body: { refreshToken },
     } = req; // refresh not expired
-
     const refreshTokenInstance = await RefreshToken.findOne({
       where: { value: refreshToken },
     });
-
-    const user = await refreshTokenInstance.getUser();
-    if (user) {
-      /* Create token pair */
-      const tokenPair = await JWTService.createTokenPair({
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      });
-      await refreshTokenInstance.update({
-        value: tokenPair.refreshToken,
-      });
-      /* prepare user */
-      const preparedUser = _.omit(user.get(), ['password']);
-      /* Send user and tokenPair */
-      return res.send({
-        data: {
-          user: preparedUser,
-          tokenPair,
-        },
-      });
-    }
+    const data = await AuthService.refreshSession(refreshTokenInstance);
+    return res.send({ data });
   } catch (error) {
     next(error);
   }
---
+++ server/src/server/services/authService.js
@@ -0,0 +1,41 @@
+const JwtService = require('./jwtService');
+const UserService = require('./userService');
+const CONSTANTS = require('../../constants');
+
+module.exports.createSession = async user => {
+  const tokenPair = await JwtService.createTokenPair({
+    id: user.id,
+    email: user.email,
+    role: user.role,
+  });
+  if ((await user.countRefreshTokens()) <= CONSTANTS.MAX_DEVICES_AMOUNT) {
+    await user.createRefreshToken({ value: tokenPair.refresh });
+  } else {
+    const [oldRefreshToken] = await user.getRefreshTokens({
+      order: [['updatedAt', 'ASC']],
+    });
+    await oldRefreshToken.update({
+      value: tokenPair.refresh,
+    });
+  }
+  return {
+    user: UserService.prepareUser(user),
+    tokenPair,
+  };
+};
+
+module.exports.refreshSession = async refreshTokenInstance => {
+  const user = await refreshTokenInstance.getUser();
+  const tokenPair = await JwtService.createTokenPair({
+    id: user.id,
+    email: user.email,
+    role: user.role,
+  });
+  await refreshTokenInstance.update({
+    value: tokenPair.refresh,
+  });
+  return {
+    user: UserService.prepareUser(user),
+    tokenPair,
+  };
+};
