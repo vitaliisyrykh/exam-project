82a85317dc713b1914dfbd2dd3d1c9830b56c806
Vladimir <freshcode.yt@gmail.com>
2021-04-29 18:59:28 +0300

    Fix router path

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/public', express.static('public'));
-app.use(router);
+app.use('/api', router);
 app.use(handlerError);
 
 const server = http.createServer(app);
