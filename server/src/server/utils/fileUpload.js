2f744c236894620cb99a009f92081e9e36668e5d
Vladimir <freshcode.yt@gmail.com>
2021-04-29 18:59:01 +0300

    Fix error handle in fileUpload

--- server/src/server/utils/fileUpload.js
+++ server/src/server/utils/fileUpload.js
@@ -35,9 +35,9 @@ const uploadLogoFiles = multer({ storage: storageContestFiles }).single(
 module.exports.uploadAvatar = (req, res, next) => {
   uploadAvatars(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      next(new ServerError());
+      next(new ServerError(err));
     } else if (err) {
-      next(new ServerError());
+      next(new ServerError(err));
     }
     return next();
   });
@@ -46,9 +46,9 @@ module.exports.uploadAvatar = (req, res, next) => {
 module.exports.uploadContestFiles = (req, res, next) => {
   uploadContestFiles(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      next(new ServerError());
+      next(new ServerError(err));
     } else if (err) {
-      next(new ServerError());
+      next(new ServerError(err));
     }
     return next();
   });
@@ -57,9 +57,9 @@ module.exports.uploadContestFiles = (req, res, next) => {
 module.exports.updateContestFile = (req, res, next) => {
   updateContestFile(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      next(new ServerError());
+      next(new ServerError(err));
     } else if (err) {
-      next(new ServerError());
+      next(new ServerError(err));
     }
     return next();
   });
@@ -68,9 +68,9 @@ module.exports.updateContestFile = (req, res, next) => {
 module.exports.uploadLogoFiles = (req, res, next) => {
   uploadLogoFiles(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      next(new ServerError());
+      next(new ServerError(err));
     } else if (err) {
-      next(new ServerError());
+      next(new ServerError(err));
     }
     return next();
   });
