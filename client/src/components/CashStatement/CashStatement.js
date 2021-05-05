32819d8f1732ad212473c8b0c13e3e1bd6d1c7b4
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-05 22:10:42 +0300

    Add arr.map and total

--- client/src/components/CashStatement/CashStatement.js
+++ client/src/components/CashStatement/CashStatement.js
@@ -2,29 +2,37 @@ import React from 'react';
 import styles from './CashStatement.module.sass';
 
 const CashStatement = props => {
+  const { arrTransactions, totalRecived, totalSpend } = props;
+
   return (
     <section className={styles.mainContainer}>
       <div className={styles.heading}>
-        <h3>
-          Cash Statement
-        </h3>
+        <h3>Cash Statement</h3>
       </div>
       <div className={styles.valuesContainer}>
         <div className={styles.valueColumn}>
           <span className={styles.headingSpan}>Id</span>
-          <span className={styles.valueSpan}>123</span>
-          <span className={styles.valueSpan}>32</span>
+          {arrTransactions.map(t => (
+            <span className={styles.valueSpan}>{t.id}</span>
+          ))}
         </div>
         <div className={styles.valueColumn}>
           <span className={styles.headingSpan}>Earned money</span>
-          <span className={styles.valueSpan}>+100</span>
-          <span className={styles.valueSpan}>+25</span>
+          {arrTransactions.map(t => (
+            <span className={styles.valueSpan}>+{t.received}</span>
+          ))}
         </div>
         <div className={styles.valueColumn}>
           <span className={styles.headingSpan}>Spent money</span>
-          <span className={styles.valueSpan}>-200</span>
-          <span className={styles.valueSpan}>-40</span>
+          {arrTransactions.map(t => (
+            <span className={styles.valueSpan}>-{t.spend}</span>
+          ))}
         </div>
+      </div>
+      <div className={styles.totalRow}>
+        <span>Total</span>
+        <span>{totalRecived}</span>
+        <span>{totalSpend}</span>
       </div>
     </section>
   );
