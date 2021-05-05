9ff969b9865ee6e10e0edc51c8e7171f9fe91123
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-05 14:32:58 +0300

    Add cashStatementPage

--- client/src/components/CashStatement/CashStatement.js
+++ client/src/components/CashStatement/CashStatement.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styles from './CashStatement.module.sass';
 
-const CashStatement = (props) => {
+const CashStatement = props => {
   return (
-    <div>
-      
-    </div>
+    <section className={styles.mainContainer}>
+      <div className={styles.heading}>
+        <h3>
+          Cash Statement
+        </h3>
+      </div>
+      <div className={styles.valuesContainer}>
+        <div className={styles.valueColumn}>
+          <span>Id</span>
+          <span>123</span>
+          <span>32</span>
+        </div>
+        <div className={styles.valueColumn}>
+          <span>Earned money</span>
+          <span>+100</span>
+          <span>+25</span>
+        </div>
+        <div className={styles.valueColumn}>
+          <span>Spent money</span>
+          <span>-200</span>
+          <span>-40</span>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default CashStatement;
