66f9d08bd21aa3a02cce7811f88fcf9aa6e21c96
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-05 14:33:21 +0300

    Add cashStatementCompon

---
+++ client/src/pages/CashStatementPage/CashStatementPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from  '../../components/Header/Header';
+import CashStatement from '../../components/CashStatement/CashStatement'
+
+const CashStatementPage = (props) => {
+  return( 
+  <div>
+    <Header/>
+    <CashStatement/>
+  </div>);
+};
+
+export default CashStatementPage;
