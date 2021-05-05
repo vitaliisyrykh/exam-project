3f41b3d3f82677afb6ead7d0de072ebecc47c0ab
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-05 22:12:45 +0300

    Add stylesTotal

--- client/src/pages/CashStatementPage/CashStatementPage.js
+++ client/src/pages/CashStatementPage/CashStatementPage.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import Header from  '../../components/Header/Header';
-import CashStatement from '../../components/CashStatement/CashStatement'
+import Header from '../../components/Header/Header';
+import CashStatement from '../../components/CashStatement/CashStatement';
 
-const CashStatementPage = (props) => {
-  return( 
-  <div>
-    <Header/>
-    <CashStatement/>
-  </div>);
+const CashStatementPage = props => {
+  const arrTransactions = [
+    { id: 5, received: 300, spend: 150 },
+    { id: 23, received: 2222, spend: 333 },
+    { id: 5, received: 300, spend: 150 },
+    { id: 23, received: 2222, spend: 333 },
+    { id: 5, received: 300, spend: 150 },
+    { id: 23, received: 2222, spend: 333 },
+    { id: 5, received: 300, spend: 150 },
+    { id: 23, received: 2222, spend: 333 },
+  ];
+  const totalRecived = 10000;
+  const totalSpend = 5000;
+  return (
+    <div>
+      <Header />
+      <CashStatement arrTransactions={arrTransactions} totalRecived={totalRecived} totalSpend={totalSpend}/>
+    </div>
+  );
 };
 
 export default CashStatementPage;
