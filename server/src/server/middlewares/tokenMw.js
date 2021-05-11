cea44e90c7e19d831c662be814494bae1fe27fd7
Vladimir <freshcode.yt@gmail.com>
2021-05-11 20:29:26 +0300

    Add auth type check

--- server/src/server/middlewares/tokenMw.js
+++ server/src/server/middlewares/tokenMw.js
@@ -7,7 +7,6 @@ module.exports.checkRefreshToken = async (req, res, next) => {
       body: { refreshToken },
     } = req;
     req.tokenData = await JwtService.verifyRefreshToken(refreshToken);
-    console.log(refreshToken)
     next();
   } catch (error) {
     next(error);
@@ -20,8 +19,11 @@ module.exports.checkAccessToken = async (req, res, next) => {
       headers: { authorization }, // Bearer asejnvr.srgrgbd.rfgdrgb
     } = req;
     if (authorization) {
-      const [, token] = authorization.split(' ');
-      console.log('token=>>>>',token);
+      const [type, token] = authorization.split(' ');
+      if(type !== 'Bearer'){
+        res.set('WWW-Authenticate', 'Bearer realm="squadhelp.com"')
+        return res.status(401).end()
+      }
       req.tokenData = await JwtService.verifyAccessToken(token);
       return next();
     }
