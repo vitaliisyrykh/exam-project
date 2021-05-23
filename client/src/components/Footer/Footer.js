ee47fa2a736fb89f750dc1bdaf05a6d1e6b5ad93
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-23 20:14:25 +0300

    Add href to map

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,13 +5,12 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   const topFooterItemsRender = item => {
-    console.log(item)
     return (
       <div key={item.title}>
         <h4>{item.title}</h4>
         {item.items.map(i => (
-          <Link to={item.href} key={i}>
-            <span>{i}</span>
+          <Link to={`${i.href}`} key={i}>
+            <span>{i.name}</span>
           </Link>
         ))}
       </div>
