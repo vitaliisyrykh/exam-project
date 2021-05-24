1c35c7fc95a0d520bfb5c93d2200f53733491d0a
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-24 18:37:21 +0300

    Add HowDoNamingContestWork component

---
+++ client/src/components/HowItWorks/HowDoNamingContestWork/HowDoNamingContestWork.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styles from './HowDoNamingContestWorks.module.sass'
+
+const HowDoNamingContestWork = () => {
+  return (
+    <section className={styles.howDoNamingContestWorkContainer}>
+      <div className={styles.heading}>
+        <span className={styles.headingText}>Our Services</span>
+        <h3 className={styles.heading}>3 Ways To Use Squadhelp</h3>
+        <span className={styles.headingDiscription}>
+          Squadhelp offers 3 ways to get you a perfect name for your business.
+        </span>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.imgContainer}></div>
+        <div className={styles.discriptionListContainer}>
+          <ul className={styles.discriptionList}>
+            <li className={styles.discriptionItem}>
+              <div className={styles.discriptionItemContainer}>
+
+              <div className={styles.discriptionItemNumber}>1.</div>
+              <span className={styles.discription}>
+                Fill out your Naming Brief and begin receiving name ideas in
+                minutes
+              </span>
+              </div>
+            </li>
+            <li className={styles.discriptionItem}>
+              <div className={styles.discriptionItemNumber}>2.</div>
+              <span className={styles.discription}>
+                Rate the submissions and provide feedback to creatives.
+                Creatives submit even more names based on your feedback.
+              </span>
+            </li>
+            <li className={styles.discriptionItem}>
+              <div className={styles.discriptionItemNumber}>3.</div>
+              <span className={styles.discription}>
+                Our team helps you test your favorite names with your target
+                audience. We also assist with Trademark screening.
+              </span>
+            </li>
+            <li className={styles.discriptionItem}>
+              <div className={styles.discriptionItemNumber}>4.</div>
+              <div className={styles.discription}>
+                Pick a Winner. The winner gets paid for their submission.
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HowDoNamingContestWork
