ed15ac8222a489f0614751906624dd995b29f2cc
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-24 12:24:28 +0300

    Add howItWorkPage

---
+++ client/src/pages/HowItWork/HowItWorkPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import HowItWork from '../../components/HowItWorks/HowItWork';
+import Footer from '../../components/Footer/Footer'
+const HowItWorkPage = () => {
+  return (
+    <>
+      <Header/>
+      <HowItWork/>
+      <Footer/>   
+    </>
+  );
+}
+
+export default HowItWorkPage;
