675523a6464a0a5e7e2de1e4e600a7a478572fdb
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-24 18:38:21 +0300

    edit classNames

--- client/src/components/HowItWorks/ThreeWaysToUseSquadHelp/ThreeWaysToUseSquadHelp.js
+++ client/src/components/HowItWorks/ThreeWaysToUseSquadHelp/ThreeWaysToUseSquadHelp.js
@@ -7,45 +7,45 @@ const ThreeWaysToUseSquadHelp = () => {
       <article className={styles.article}>
         <span className={styles.headingText}>Our Services</span>
         <h3 className={styles.heading}>3 Ways To Use Squadhelp</h3>
-        <span className={styles.text}>
+        <span className={styles.headingDiscription}>
           Squadhelp offers 3 ways to get you a perfect name for your business.
         </span>
       </article>
       <section className={styles.cardsContainer}>
-        <div>
+        <div className={styles.card}>
           <img />
-          <h3>Launch a Contest</h3>
-          <span>
+          <h3 className={styles.cardHeading}>Launch a Contest</h3>
+          <span className={styles.cardDescription}>
             Work with hundreds of creative experts to get custom name
             suggestions for your business or brand. All names are auto-checked
             for URL availability.
           </span>
-          <Link>
-            <button>Launch a Contest</button>
+          <Link to="/startContest">
+            <button className={styles.linkBtn}>Launch a Contest</button>
           </Link>
         </div>
-        <div>
+        <div className={styles.card}>
           <img />
-          <h3>Explore Names For Sale</h3>
-          <span>
+          <h3 className={styles.cardHeading}>Explore Names For Sale</h3>
+          <span className={styles.cardDescription}>
             Our branding team has curated thousands of pre-made names that you
             can purchase instantly. All names include a matching URL and a
             complimentary Logo Design
           </span>
-          <Link>
-            <button>Explore Names For Sale</button>
+          <Link to="google.com">
+            <button className={styles.linkBtn}>Explore Names For Sale</button>
           </Link>
         </div>
-        <div>
+        <div className={styles.card}>
           <img />
-          <h3>Agency-level Managed Contests</h3>
-          <span>
+          <h3 className={styles.cardHeading}>Agency-level Managed Contests</h3>
+          <span className={styles.cardDescription}>
             Our Managed contests combine the power of crowdsourcing with the
             rich experience of our branding consultants. Get a complete
             agency-level experience at a fraction of Agency costs
           </span>
-          <Link>
-            <button>Learn more</button>
+          <Link to="google.com">
+            <button className={styles.linkBtn}>Learn more</button>
           </Link>
         </div>
       </section>
