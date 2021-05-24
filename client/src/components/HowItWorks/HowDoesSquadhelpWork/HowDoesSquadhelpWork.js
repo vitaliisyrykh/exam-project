9044d725559b99d967535929c3c6c03d7b40cfc3
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-24 12:24:11 +0300

    Add styles to HowDoesSquadhelp component

--- client/src/components/HowItWorks/HowDoesSquadhelpWork/HowDoesSquadhelpWork.js
+++ client/src/components/HowItWorks/HowDoesSquadhelpWork/HowDoesSquadhelpWork.js
@@ -1,22 +1,27 @@
 import React from 'react'
-
+import styles from './HowDoesSquadhelpWork.module.sass'
 const HowDoesSquadhelpWork = props => {
   return (
-    <div className='howDoesSquadhelpWorkMainContainer'>
-      <div className='flexContainer'>
-        <div className='textAndVideoHowDoesSquadhelpWork'>
-          <span>Word`s #1 Naming Platform </span>
-          <h3>How Does Squadhelp Work?</h3>
-          <p>
+    <section className={styles.howDoesSquadhelpWorkMainContainer}>
+      <div className={styles.flexContainer}>
+        <article className={styles.textAndVideoHowDoesSquadhelpWork}>
+          <span className={styles.raitingSpan}>Word`s #1 Naming Platform </span>
+          <h3 className={styles.heading}>How Does Squadhelp Work?</h3>
+          <p className={styles.paragraph}>
             Squadhelp helps you come up with a great name for your business by
             combining the power of crowdsourcing with sophisticated technology
             and Agency-level validation services.
           </p>
-           <video width='750' height='500' controls>
-            <source src="https://vimeo.com/368584367"/></video>
-        </div>
+          <button className={styles.playBtn}>
+            <svg width='28' height='28' viewBox='0 0 24 24' fill='white'>
+              <path d='M8,5.14V19.14L19,12.14L8,5.14Z' />
+            </svg>
+            <span>Play Video</span>
+          </button>
+        </article>
+        <div className='imgContainer'></div>
       </div>
-    </div>
+    </section>
   )
 }
 
