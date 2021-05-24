f8081859b0678dda4a1fa489d51def90e95bc383
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-05-24 18:36:16 +0300

    Add HowDoNamingWork

--- client/src/components/HowItWorks/HowItWork.js
+++ client/src/components/HowItWorks/HowItWork.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HowDoesSquadhelpWork from './HowDoesSquadhelpWork/HowDoesSquadhelpWork';
-import ThreeWaysToUseSquadHelp from './ThreeWaysToUseSquadHelp/ThreeWaysToUseSquadHelp'
+import ThreeWaysToUseSquadHelp from './ThreeWaysToUseSquadHelp/ThreeWaysToUseSquadHelp';
+import HowDoNamingContestWork from './HowDoNamingContestWork/HowDoNamingContestWork';
 
 const HowItWork = props => {
   return (
     <div>
       <HowDoesSquadhelpWork/>
       <ThreeWaysToUseSquadHelp/>
+      <HowDoNamingContestWork/>
     </div>
   );
 }
