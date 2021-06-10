b7f61fc9d7f53ef24fae60f526bf1d7cd340ac0e
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-06-10 14:56:23 +0300

    Add formatCode

--- client/src/components/HowItWorks/ReadyToGetStarted/ReadyToGetStarted.js
+++ client/src/components/HowItWorks/ReadyToGetStarted/ReadyToGetStarted.js
@@ -4,7 +4,7 @@ import styles from './ReadyToGetStarted.module.sass';
 
 const ReadyToGetStarted = props => {
   return (
-    <section >
+    <section>
       <div className={styles.mainContainerGetStarted}>
         <div className={styles.containerGetStarted}>
           <h3 className={styles.headingGetStarted}>Ready To Get Started</h3>
@@ -20,15 +20,25 @@ const ReadyToGetStarted = props => {
       <div className={styles.progressContainer}>
         <div className={styles.progress}>
           <img src='https://www.squadhelp.com/resources/assets/imgs/front/stars.svg' />
-          <p><span>4.9 out of 5 stars</span> from 25,000+ customers.</p>
+          <p>
+            <span>4.9 out of 5 stars</span> from 25,000+ customers.
+          </p>
         </div>
-        <div className={styles.progress} >
-          <img src='https://www.squadhelp.com/resources/assets/imgs/front/img2(1).png'/>
-          <p>Our branding community stands <span>200,000+</span> strong</p>
+        <div className={styles.progress}>
+          <img src='https://www.squadhelp.com/resources/assets/imgs/front/img2(1).png' />
+          <p>
+            Our branding community stands <span>200,000+</span> strong
+          </p>
         </div>
-        <div className={styles.progress } >
-          <img src='https://www.squadhelp.com/resources/assets/imgs/front/sharing-files.svg' className={styles.modifImg}/>
-          <p className={styles.modif}><span>140+ Industries </span> supported across more than <span>85 countries</span> – and counting.</p>
+        <div className={styles.progress}>
+          <img
+            src='https://www.squadhelp.com/resources/assets/imgs/front/sharing-files.svg'
+            className={styles.modifImg}
+          />
+          <p className={styles.modif}>
+            <span>140+ Industries </span> supported across more than{' '}
+            <span>85 countries</span> – and counting.
+          </p>
         </div>
       </div>
     </section>
